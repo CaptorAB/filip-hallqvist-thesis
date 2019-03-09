0be2c11cdd12e940071f48037c133640d2b7529f
Filip Hallqvist <filiphallqvist@gmail.com>
2019-03-09 17:41:24 +0100

    Initial work on capgen

--- README.md
+++
@@ -1,7 +0,0 @@
-# Constrained Portfolio Optimization in Liability-Driven Investing
-
-This repository contains all the code relevant to my master's thesis. The thesis work is an ongoing project, but the end date is set to June 2019.
-
-## License
-
-MIT
---
+++ app/index.js
@@ -0,0 +1,11 @@
+import init from "../bin/libcapgen.js";
+import wasm from "../bin/libcapgen.wasm";
+
+const libcapgen = init({
+  locateFile: path => (path.endsWith("libcapgen.wasm") ? wasm : path)
+});
+
+libcapgen.onRuntimeInitialized = () => {
+  window.libcapgen = libcapgen;
+  window.foo = libcapgen.cwrap("foo", "number", ["number"]);
+};
