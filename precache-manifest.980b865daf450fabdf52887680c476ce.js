d3777c9421dff2b7a2bbfb2c3dbda52a254055a3
Filip Hallqvist <filiphallqvist@gmail.com>
2019-04-19 15:40:13 +0200

    Updates

---
+++ precache-manifest.980b865daf450fabdf52887680c476ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "665dec265a359b798ebf",
+    "url": "/filip-hallqvist-thesis/static/css/main.cb7eab0f.chunk.css"
+  },
+  {
+    "revision": "665dec265a359b798ebf",
+    "url": "/filip-hallqvist-thesis/static/js/main.769d1789.chunk.js"
+  },
+  {
+    "revision": "9072af8340fcba088608",
+    "url": "/filip-hallqvist-thesis/static/js/runtime~main.cbe46372.js"
+  },
+  {
+    "revision": "c05aaa383863f25dbbbf",
+    "url": "/filip-hallqvist-thesis/static/js/2.a9475b41.chunk.js"
+  },
+  {
+    "revision": "a228e37c621c06fa44cac9cd9afbce97",
+    "url": "/filip-hallqvist-thesis/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/filip-hallqvist-thesis/precache-manifest.f8b3fa81cabd3800ef9aa4140b108253.js"
+  "/filip-hallqvist-thesis/precache-manifest.980b865daf450fabdf52887680c476ce.js"
 );
 
 workbox.clientsClaim();
